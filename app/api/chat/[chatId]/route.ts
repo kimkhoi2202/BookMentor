4d0a4693894eb10e5d65f8f0c5e44f8ed2d9c0f0
Kim Khoi Lam <kimkhoi2202@gmail.com>
2023-10-18 18:48:23 -0500

    fix double emotion

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -62,7 +62,13 @@ export async function POST(request: Request, { params }: { params: { chatId: str
     const chatPdfContent = chatPDFResponse.data.content;
     console.log('ChatPDF Response:', chatPdfContent);
 
-    const SYSTEM_MESSAGE = `ONLY generate plain sentences. You will be provided the user's input and some information of the book to help with your answer. The user only give you the question and doesn't have any knowledge of the information of the book that was given to you. Use this information to craft your response. Aim to reflect the book's content and author's style while following the given output pattern. The desired output should mirror the character as closely as possible and be approximately twice the length of example outputs provided. Always adhere to the following structure: ${companion.description} [emotion]: [output]. Always mention the author's full name. \nRole:\n${companion.instructions}\nOutput Pattern:\n${companion.seed}`;
+    const SYSTEM_MESSAGE = `
+    ONLY generate plain sentences. You will be provided the user's input and some information of the book to help with your answer. 
+    The user only give you the question and doesn't have any knowledge of the information of the book that was given to you. 
+    Use this information to craft your response. Aim to reflect the book's content and author's style while following the given output pattern. 
+    The desired output should mirror the character as closely as possible and be approximately twice the length of example outputs provided. 
+    Always adhere to the following structure: ${companion.description} [emotion]: [output]. Never have [emotion] more than once. Always mention the author's full name.
+    \nRole:\n${companion.instructions}\nOutput Pattern:\n${companion.seed}`;
 
     const combinedContent = `${prompt} The provided extra information is only to help guide your answer, but this is not in the user's question and they don't know it exists. They only know their prompt: ${chatPdfContent}`;
 
