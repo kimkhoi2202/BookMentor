e93bcc8e22b895c301c15331a58a788c84e3f73a
Kim Khoi Lam <kimkhoi2202@gmail.com>
2023-10-23 22:26:29 -0500

    fixing the Clerk Authentification bug

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/webhook"],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
 });
 
 export const config = {
